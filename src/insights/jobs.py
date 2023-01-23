214e7bdd585caaa33d58f82b4e60ae94f45de6b9
Gabriel <gabrielja2antunes@gmail.com>
2023-01-23 17:26:15 -0300

    fix: lint função read

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -6,14 +6,14 @@ import csv
 @lru_cache
 def read(path: str) -> List[Dict]:    
     with open(path, mode="r") as file:
-            DictReader_obj = csv.DictReader(file)
-            return [row for row in DictReader_obj]    
+        DictReader_obj = csv.DictReader(file)
+        return [row for row in DictReader_obj]    
 
 
 def get_unique_job_types(path: str) -> List[str]:
-   obj = read(path)
-   for item in obj:
-    return item['job_type']
+    obj = read(path)
+    for item in obj:
+     return item['job_type']
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
