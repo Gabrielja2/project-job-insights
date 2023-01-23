41a3a7b9aceda9dd71c985cf07511dbdb6d46a8a
Gabriel <gabrielja2antunes@gmail.com>
2023-01-23 18:38:28 -0300

    fix: lint módulo industries.py

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 from src.insights.jobs import read
 
+
 def get_unique_industries(path: str) -> List[str]:
     readedfile = read(path)
     industrie_set = set()
@@ -10,6 +11,7 @@ def get_unique_industries(path: str) -> List[str]:
         else:
             industrie_set.add(industrie['industry'])
     return list(industrie_set)
+    
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     """Filters a list of jobs by industry
