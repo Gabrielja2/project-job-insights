77d533050d069d680544c1ce43103ae9cd72ec92
Gabriel <gabrielja2antunes@gmail.com>
2023-01-23 17:22:59 -0300

    feat: função read finalizada

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,40 +1,19 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
-def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+def read(path: str) -> List[Dict]:    
+    with open(path, mode="r") as file:
+            DictReader_obj = csv.DictReader(file)
+            return [row for row in DictReader_obj]    
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+   obj = read(path)
+   for item in obj:
+    return item['job_type']
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
