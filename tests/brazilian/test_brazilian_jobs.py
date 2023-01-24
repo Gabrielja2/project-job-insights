74c0940c68d78fb577bc06f198ccc1bf607ef529
Gabriel <gabrielja2antunes@gmail.com>
2023-01-24 15:12:33 -0300

    test:  testando a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    results = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert results.__contains__(
+        {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
+    )
