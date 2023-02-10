480e94f01f9bb19ffaa997022d95e84a34bec3a6
Gabriel <gabrielja2antunes@gmail.com>
2023-02-10 18:43:34 -0300

    🛠️ refactoring: remove trybe files

--- tests/brazilian/test_brazilian_jobs.py
+++
@@ -1,8 +0,0 @@
-from src.pre_built.brazilian_jobs import read_brazilian_file
-
-
-def test_brazilian_jobs():
-    results = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
-    assert results.__contains__(
-        {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'}
-    )
